05c8a837b90f53da1fc145a3c7046155f2fea80f
HowToCompute <howtocompute123@gmail.com>
2016-06-18 12:26:18 +0200

    Revert "Functions!"
    This reverts commit 06ff8818884811452b1630339b03a6dff70e3b3a.

--- HangMan/Main.cpp
+++ HangMan/Main.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
 #include <string>
-
-void WelcomeUser();
-void PlayGame();
-
 int main() {
-	WelcomeUser();
-	PlayGame();
-}
-
-void WelcomeUser() {
 	//Welcome the user to the game
 	std::cout << "Hello and welcome to HangMan!\nThis is a wordguessing game where you have to guess the word with a maximum ammount of wrong guesses.\nIf you have too many wrong guesses you DIE!" << std::endl;
-	std::cout << "Press enter to play!" << std::endl;
+	std::cout << "Press enter to continue!" << std::endl;
 	std::cin.ignore();
-	return;
-}
 
-void PlayGame() {
-	std::cout << "Hello, I am the game function!" << std::endl;
 	//Check if the user has any lives left
-		//Yes
-			//Ask for input
-	//Is the input valid
+	//Yes
+		//Ask for input
+		//Is the input valid
 		//Yes
 			//Check against the word
 			//Return the result to the user
@@ -30,9 +17,9 @@ void PlayGame() {
 		//No
 			//Give an error
 			//Goto "Check if the user has any lives left"
-		//No
-			//Tell the user they are dead
-			//Ask the user if they want to play again
+	//No
+		//Tell the user they are dead
+		//Ask the user if they want to play again
 		//Yes
 			//GOTO "Welcome the user to the game"
 		//No
